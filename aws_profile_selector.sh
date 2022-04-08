d4ab032cecc3cc8beb8d275e5f85f178c8c7054c
Akshay Jain <akshayjain.ecb@gmail.com>
2022-04-08 15:25:57 +0200

    Create aws_profile_selector.sh

---
+++ aws_profile_selector.sh
@@ -0,0 +1,20 @@
+##AWS Profile Selector
+## Add below function in your .bashrc or .bash_profile and then call function aws_p,  you will see a interactive menu to set the desired AWS PROFILE
+function aws_p() {
+    # AWS Profile Selector
+    IFS=$'\n' read -d '' -ra lines < <(grep "\[profile" ~/.aws/config  | cut -d" " -f 2 | tr -d "]")
+    echo -e "\033[1;34m - - - - - - - - - - - - - - - \033[0m"
+    select choice in "${lines[@]}"; do
+    [[ -n $choice ]] || { echo "Invalid choice. Please try again." >&2; continue; }
+    break # valid choice was made; exit prompt.
+    done
+    read -r id  <<<"$choice"
+    echo -e "\033[1;32m $id \033[0m"
+    export AWS_PROFILE=$id
+    aws sts get-caller-identity
+    echo -e "\033[1;34m - - - - - - - - - - - - - - - \033[0m"
+}
+
+export -f aws_p
+
+## To use this, simply 
